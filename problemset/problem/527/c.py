5ef866bd69b507b9262a33ed910f60475f88e950
x1- <viva008@gmail.com>
2015-03-24 19:41:39 +0900

    heapqをもっと使うように修正中。

--- problemset/problem/527/c.py
+++ problemset/problem/527/c.py
@@ -59,19 +59,31 @@ H 3
 9
 6
 
+h = []
+for i in [(7,15),(3, 22),(9, 4),(0, 4),(2, 15),(3, 16),(2, 2),(5, 6)]:
+  heapq.heappush( h, i )
+
+for i in xrange(len(h)):
+  print heapq.heappop(h)
+
+sys.exit
 """
 
 sizes = map(int, raw_input().split())
 
 #start = time.clock()
 
+ct =    { 'H': sizes[2], 'V': sizes[2] }
 dt =    { 'H': [], 'V': [] }
-diffs = { 'H': [sizes[1]], 'V': [sizes[0]] }
+diffs = { 'H': [], 'V': [] }
 
 heapq.heappush( dt['V'], 0 )
 heapq.heappush( dt['V'], sizes[0] )
 heapq.heappush( dt['H'], 0 )
 heapq.heappush( dt['H'], sizes[1] )
+
+heapq.heappush( diffs['V'], (sizes[2], sizes[0]) )
+heapq.heappush( diffs['H'], (sizes[2], sizes[1]) )
 
 for x in xrange(sizes[2]):
   (k, v) = raw_input().split()
@@ -80,10 +92,36 @@ for x in xrange(sizes[2]):
   heapq.heappush( dt[k], pos )
 
   dt[k] = [heapq.heappop(dt[k]) for i in xrange(len(dt[k]))]
-  diffs[k] = list(dt[k])
-  diffs[k] = [diffs[k][i] - diffs[k][i-1] for i in xrange(len(diffs[k])-1, 0, -1)]
+  for i in xrange(len(dt[k])-2, 0, -1):
+    if dt[k][i] == pos:
+      diff1 = dt[k][i+1] - pos
+      diff2 = pos - dt[k][i-1]
+      diff = diff1 + diff2
 
-  print heapq.nlargest(1, diffs['H']).pop() * heapq.nlargest(1, diffs['V']).pop()
+      tmp = []
+      for j in xrange(len(diffs[k])):
+        x = heapq.heappop( diffs[k] )
+        heapq.heappush( tmp, x )
+        if x[1] == diff:
+          heapq.heappush( tmp, (x[0]-1, diff1) )
+          heapq.heappush( tmp, (x[0]-1, diff2) )
+          ct[k] -= 1
+
+      diffs[k] = tmp
+#      for j in xrange(len(diffs[k])):
+#        if diffs[k][j] == diff:
+#          del diffs[k][j]
+#          break
+
+#      diffs[k].append( diff1 )
+#      diffs[k].append( diff2 )
+      break
+
+#  print sorted(diffs['H'], reverse=True)[0] * sorted(diffs['V'], reverse=True)[0]
+
+  print diffs
+#  print sorted(diffs['H'], cmp=lambda x, y: x[0] < y[0] and x[1] > y[1])
+#  print sorted(diffs['V'], cmp=lambda x, y: x[0] < y[0] and x[1] > y[1])
 
 #end = time.clock()
 #print end - start
