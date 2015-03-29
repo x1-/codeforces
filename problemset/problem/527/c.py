2448a3137d3d9f9738283e09d7f56a5a1aa56c1b
yuri_inoue <viva008@gmail.com>
2015-03-29 19:17:46 +0900

    少しだけ高速化

--- problemset/problem/527/c.py
+++ problemset/problem/527/c.py
@@ -1,51 +1,11 @@
 import sys
-import time
-import bisect
+#import time
 from array import array
 
-def search( xs, target ):
-  high = len(xs)-1
-  low = 1
-  idx = high/2
-
-  if target < xs[low]:
-    return low
-  elif target > xs[high-1]:
-    return high
-
-  while(low<high):
-    if xs[idx] > target and target > xs[idx-1]:
-      break
-    elif target > xs[idx]:
-      low = idx + 1
-    elif target < xs[idx]:
-      high = idx - 1
-
-    idx = (high + low)/2
-
-  return idx
-
-def count( xs, pos, size, back=True ):
-  if back:
-    add = 1
-    n = size - pos
-  else:
-    add = -1
-    n = pos
-
-  counter = 1
-  idx = pos + add
-
-  while(n > 0):
-    if xs[idx] == 1:
-      break
-    idx = idx + add
-    n -= 1
-    counter += 1
-
-  return counter
-
 def incr_dict( dt, key ):
+  if key == 1:
+    return dt
+
   v = dt.setdefault( key, 0 )
   dt[key] += 1
   return dt
@@ -117,45 +77,71 @@ H 3
 
 sizes = map(int, raw_input().split())
 
-start = time.clock()
+#start = time.clock()
 
+a = array('I', [0 for _ in xrange(sizes[2])])
+t = array('c', [' ' for _ in xrange(sizes[2])])
 
-s    =  { 'H': sizes[1], 'V': sizes[0] }
-#dt   =  { 'H': array('I', [0, sizes[1]]), 'V': array('I', [0, sizes[0]]) }
-#dt   =  { 'H': array('I', [0] * (sizes[1]+1)), 'V': array('I', [0] * (sizes[0]+1)) }
-dt   =  { 'H': array('I', [0 for _ in xrange(sizes[1]+1)]), 'V': array('I', [0 for _ in xrange(sizes[0]+1)]) }
-sets =  { 'H': {sizes[1]: 1}, 'V': {sizes[0]: 1} }
+dtV = array('B', [0 for _ in xrange(sizes[0]+1)])
+dtV[0] = 1
+dtV[sizes[0]] = 1
+dtH = array('B', [0 for _ in xrange(sizes[1]+1)])
+dtH[0] = 1
+dtH[sizes[1]] = 1
 
-dt['V'][0] = 1
-dt['V'][sizes[0]] = 1
-dt['H'][0] = 1
-dt['H'][sizes[1]] = 1
+setV = {sizes[0]: 1, 1: 1}
+setH = {sizes[1]: 1, 1: 1}
 
-for x in xrange(sizes[2]):
+N = sizes[2]
+
+for x in xrange(N):
   (k, v) = raw_input().split()
 
-  pos = int(v)
+  t[x] = k
+  a[x] = int(v)
 
-#12.124999
-#  idx = bisect.bisect_left(dt[k], pos, lo=1, hi=len(dt[k])-1)
+# ----------------------------------------------------------
+for x in xrange(N):
 
-#  diff1 = dt[k][idx] - pos
-#  diff2 = pos - dt[k][idx-1]
-#  diff = diff1 + diff2
+  pos = a[x]
 
+  f = 1
+  b = 1
+  if t[x] == 'V':
+    while( True ):
+      if dtV[pos-f] == 1:
+        break
+      f += 1
+    while( True ):
+      if dtV[pos+b] == 1:
+        break
+      b += 1
+    dtV[pos] = 1
 
-#  dt[k].insert( idx, pos )
+    decr_dict( setV, b + f )
+    incr_dict( setV, f )
+    incr_dict( setV, b )
+  else:
+    while( True ):
+      if dtH[pos-f] == 1:
+        break
+      f += 1
 
-  back  = count( dt[k], pos, s[k], back=True )
-  front = count( dt[k], pos, s[k], back=False )
+    while( True ):
+      if dtH[pos+b] == 1:
+        break
+      b += 1
+    dtH[pos] = 1
 
-  dt[k][pos] = 1
+    decr_dict( setH, b + f )
+    incr_dict( setH, f )
+    incr_dict( setH, b )
 
-  decr_dict( sets[k], back + front )
-  incr_dict( sets[k], front )
-  incr_dict( sets[k], back )
+  a[x] = max(setH.keys()) * max(setV.keys())
 
-  print max(sets['H'].keys()) * max(sets['V'].keys())
+#end = time.clock()
 
-end = time.clock()
-print end - start
+for ans in a:
+  print ans
+
+#print end - start
