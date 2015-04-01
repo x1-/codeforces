fef386758a668ac746a10f9656660b0aa4c8aaa1
yuri_inoue <viva008@gmail.com>
2015-04-01 23:41:37 +0900

    337, 作りかけ

--- problemset/problem/337/a.py
+++ problemset/problem/337/a.py
@@ -7,8 +7,8 @@ A. Puzzles
 time limit per test1 second
 memory limit per test256 megabytes
 
-n, m (2 ≤ n ≤ m ≤ 50)
-f1, f2, ..., fm (4 ≤ fi ≤ 1000)
+n, m(2<=n<=m<=50)
+f1,f2,...,fm(4<=fi<=1000)
 
 #input1:
 4 6
@@ -18,9 +18,23 @@ f1, f2, ..., fm (4 ≤ fi ≤ 1000)
 5
 """
 nm     = map(int, raw_input().split())
-pieces = map(int, raw_input().split())
+pieces = array('I', [ int(x) for x in raw_input().split() ])
 
+start = time.clock()
+
+n = nm[1] - nm[0] + 1
+spieces = sorted( pieces, reverse=True )
+print spieces
+min_diff = spieces[0] - spieces[n-1]
+print min_diff
+for i in xrange(n):
+  diff = spieces[i] - spieces[i+n-1]
+  print "d", diff
+  if diff < min_diff:
+    min_diff = diff
 
 end = time.clock()
 print end - start
+
+print min_diff
 sys.exit(0)
