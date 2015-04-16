70979548022dd0dfa2fccf1ca7286b8a600dd1a3
yuri_inoue <viva008@gmail.com>
2015-04-16 23:40:09 +0900

    また修正。。。

--- problemset/problem/526/a.py
+++ problemset/problem/526/a.py
@@ -6,17 +6,15 @@ ds = list(raw_input())
 
 start = time.clock()
 
-q = n / 4
 res = 'no'
 
-for i in xrange(n):
+for i in xrange(n-3):
 
-  for j in xrange(4, 6):
-    quoter = n / j
+  q = (n-i) / 4
+  for j in xrange(q, q+2):
     fall = False
     count = 0
-
-    for k in xrange(i, n, quoter):
+    for k in xrange(i, n, j):
       #print k, ds[k]
       if ds[k] == '*':
         count += 1
@@ -27,7 +25,6 @@ for i in xrange(n):
     if count == 4 and not fall:
       res = 'yes'
       break
-
   if res == 'yes':
     break
 
