0418a97230ace0a763bedf53770e5cac4c7e2e16
x1- <viva008@gmail.com>
2015-04-17 19:59:12 +0900

    なかなか思うように動かない。

--- problemset/problem/526/a.py
+++ problemset/problem/526/a.py
@@ -2,7 +2,7 @@ import sys
 import time
 
 n = input()
-ds = list(raw_input())
+ds = list(raw_input() + "*")
 
 start = time.clock()
 
@@ -11,18 +11,20 @@ res = 'no'
 for i in xrange(n-3):
 
   q = (n-i) / 4
-  for j in xrange(q, q+2):
+  #if q <= i:
+  #  break
+  for j in xrange(q, q+i+1):
     fall = False
     count = 0
-    for k in xrange(i, n, j):
-      #print k, ds[k]
+    for k in xrange(i, n+1, j):
+      print k, ds[k]
       if ds[k] == '*':
         count += 1
       else:
         fall = True
         break
 
-    if count == 4 and not fall:
+    if count == 5 and not fall:
       res = 'yes'
       break
   if res == 'yes':
