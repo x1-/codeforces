6917d53bb3401ee11720cf4d1cde9f7d55a281a1
x1- <viva008@gmail.com>
2015-05-15 19:58:01 +0900

    heapqのベンチマークを追加しました。

---
+++ time/pop_vs_heappop.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#-*- encoding: utf-8 -*-
+
+import sys
+from benchmarker import Benchmarker
+from heapq
+
+n = int(sys.argv[1]) if len(sys.argv) > 1 else 1000*1000
+
+with Benchmarker(n, width=20) as bench:
+    nay = [i for i in xrange(n)]
+    iay = array('I', [i for i in xrange(n)])
+
+    @bench("list")
+    def _(bm):
+        for i in xrange(n):
+            nay.pop(0)
+
+    @bench("array")
+    def _(bm):
+        for i in xrange(n):
+            iay.pop(0)
